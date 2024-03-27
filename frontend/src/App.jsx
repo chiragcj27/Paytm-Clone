4484bf333bf3673f16c3d5f6ae8b5251e0e1e2cf
Chirag Jain <cj27chirag@gmail.com>
2024-03-27 16:14:09 +0530

    frontend components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import {BrowserRouter, Routes} from 'react-router-dom'
-import './App.css'
+import { Signup } from './pages/Signup'
 
 function App() {
   return (
     <>
-       <BrowserRouter>
+       {/* <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Signup/>
     </>
   )
 }
