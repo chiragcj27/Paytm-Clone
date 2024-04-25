276cc8e42e8616e2bbae96b38e01e3da6ecc6310
Chirag Jain <cj27chirag@gmail.com>
2024-04-25 13:35:37 +0530

    added transaction successful page

--- frontend/src/pages/Transaction.jsx
+++ frontend/src/pages/Transaction.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 import tick from '../assets/7efs.gif'
 import { Button } from '../components/Button'
+import gian from '../assets/gian-hain-aap.mp3'
 
 export const Transaction = () => {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const audio = new Audio(gian);
+        audio.play();
+        
+        // Clean up the audio element when the component unmounts
+        return () => {
+          audio.pause();
+          audio.currentTime = 0;
+        };
+      }, []); 
+
     return <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
